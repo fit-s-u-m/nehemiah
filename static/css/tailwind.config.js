f6080a66a52aa34516692e4d0347920daf3be621
fit-s-u-m <nehemiahmaths@gmail.com>
2024-03-22 23:43:30 +0300

    first commit htmx+ go but confused

---
+++ static/css/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "/views/home.html",
+    "../../views/index.html",
+    "../../views/*"
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [ 
+    ],
+}
+

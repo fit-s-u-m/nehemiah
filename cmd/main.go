40f482bd47ba5fc4afc944b76c03044ede019bd9
fit-s-u-m <nehemiahmaths@gmail.com>
2024-03-24 00:44:18 +0300

    first stating point

--- cmd/main.go
+++ cmd/main.go
@@ -21,24 +21,6 @@ func newTemplate() *Templates {
 	}
 }
 
-type blog_list struct {	
-	Title string
-	Content string
-}
-bloglists := [3]blog_list{ 
-	blog_list{
-		Title: "Trigonometry",
-		Content: "Trigonometry is a branch of mathematics that studies relationships between the sides and angles of triangles."
-	}, 
-	blog_list{
-		Title: "Title 2", 
-		Content: "Content 2"
-	}, 
-	blog_list{Title: "Title 3", Content: "Content 3"}
-}
-
-
-
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
@@ -46,8 +28,8 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(200, "home.html", nil)
 	})
-	e.POST("/blogs", func(c echo.Context) error {
-		return c.Render(200, "blog.html", blog_lists)
+	e.GET("/blogs", func(c echo.Context) error {
+		return c.Render(200, "blog.html", nil)
 	})
 	e.Static("/static", "static")
 	e.Logger.Fatal(e.Start(":8080"))
